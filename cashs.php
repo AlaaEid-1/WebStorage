78fb5f8d600af31ecbbed6901cdf6e7fb4466302
alaasamieid <61549252+AlaaEid-1@users.noreply.github.com>
2022-10-28 22:56:23 +0300

    cashes.php

---
+++ cashs.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>cash memory</title>
+        <link href="libs/font-awesome-4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+        <!-- <link href="createStyle.css" rel="stylesheet" type="text/css"/> -->
+    </head>
+    <body>  
+            <h2>cash memory</h2>
+            <form class="empform" id="empform" action="" enctype="multipart/form-data">   
+           <div> 
+            <label>capcity:</label>
+            <input type="text" name="capcity" id="capcity" /> <br>
+                   
+</div><br><br>
+<div>
+    <label>Hit</label>
+    <input type="text" name="Hit" id="Hit" /> <br>
+                    
+</div><br><br>
+<div>
+    <label>Miss</label>
+                    <input type="text" name="Miss" id="Miss" /> <br>
+                    
+                </div>
+        </form>
+        
+
+        <script src="js/jquery-3.6.0.min.js" type="text/javascript"></script>
+        <script src="js/myscript.js" type="text/javascript"></script>
+
+    </body>
+</html>
+
+
+
+
